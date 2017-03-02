1efd5087db41f0c5af5458c101192d7c30c43edf
ineeve <renatoineeve@gmail.com>
2017-03-02 14:37:14 +0000

    dont know

--- dkeep/logic/maps/KeepMap.java
+++ dkeep/logic/maps/KeepMap.java
@@ -1,9 +1,13 @@
 package dkeep.logic.maps;
 
+import dkeep.logic.Hero;
+import java.awt.Point;
+
 /**
  * Created by João on 23/02/2017.
  */
 public class KeepMap extends GameMap{
+    private static Point heroPos = new Point(1,1);
     public KeepMap() {
         map = new char[][] {
                 { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
@@ -15,6 +19,8 @@ public class KeepMap extends GameMap{
                 { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
                 { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
                 { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' } };
+        
+        agents.add(new Hero(heroPos));
     }
 
     @Override
