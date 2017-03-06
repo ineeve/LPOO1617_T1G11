1b9d0327802938c400d06663362767b129422273
ineeve <renatoineeve@gmail.com>
2017-03-06 00:00:04 +0000

    Added test

---
+++ dkeep/test/TestDungeonGameLogic.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import dkeep.logic.Game;
+import dkeep.logic.maps.GameMap;
+import dkeep.logic.maps.Task1TestMap;
+import java.awt.Point;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestDungeonGameLogic {
+    char map[][] = {{'X','X','X','X','X'},
+                    {'X',' ',' ',' ','X'},
+                    {'I',' ',' ',' ','X'},
+                    {'I',' ',' ',' ','X'},
+                    {'X','X','X','X','X'}};
+    
+    @Test
+    public void testMoveHeroIntoFreeCell(){
+        GameMap gameMap = new Task1TestMap(map);
+        Game game = new Game(gameMap);
+        assertEquals(new Point(1,1),game.getHeroPos());
+        game.update();
+        assertEquals(new Point(1,2), game.getHeroPos());
+    }
+}
