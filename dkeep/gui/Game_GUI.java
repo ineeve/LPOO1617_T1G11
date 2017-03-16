8c283b9e908ad4d229e7c9b9ca56637eced3fc48
ineeve <renatoineeve@gmail.com>
2017-03-16 14:27:00 +0000

    Modified textArea to panel

--- dkeep/gui/Game_GUI.java
+++ dkeep/gui/Game_GUI.java
@@ -170,7 +170,7 @@ public class Game_GUI {
 		});
 		frmEscapeGame.getContentPane().add(btnNewGame, "cell 6 2 5 1,sizegroupx 2,alignx center,sizegroupy 2,aligny center");
 
-		scrollPane = new JScrollPane();
+		/*scrollPane = new JScrollPane();
 		frmEscapeGame.getContentPane().add(scrollPane, "cell 0 4 5 10,grow");
 
 
@@ -178,7 +178,11 @@ public class Game_GUI {
 		textArea = new JTextArea();
 		textArea.setFont(new Font("Courier New",Font.PLAIN,14));
 		textArea.setEditable(false);
-		scrollPane.setViewportView(textArea);
+		scrollPane.setViewportView(textArea);*/
+		
+		JPanel graphicsPanel = new SimpleGraphicsPanel(); 
+		frmEscapeGame.getContentPane().add(graphicsPanel,"cell 0 4 5 10,grow");
+		graphicsPanel.repaint();
 
 		panel = new JPanel();
 		frmEscapeGame.getContentPane().add(panel, "cell 7 5 5 4,alignx center");
---
+++ dkeep/gui/SimpleGraphicsPanel.java
@@ -0,0 +1,79 @@
+package dkeep.gui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JPanel;
+
+public class SimpleGraphicsPanel extends JPanel  implements MouseListener, MouseMotionListener, KeyListener { 
+	// Coordinates of the elipse “bounding rectangle”
+	private int x1 = 0, y1 = 0, x2 = 0, y2 = 0; 
+	// Constructor, adding mouse and keyboard listeneres 
+	public SimpleGraphicsPanel() { 
+		addMouseListener(this); 
+		addMouseMotionListener(this); 
+		addKeyListener(this); 
+	}
+
+	// Redraws the panel, only when requested by SWING
+	public void paintComponent(Graphics g) { 
+		super.paintComponent(g); // limpa fundo ... 
+		g.setColor(Color.BLUE); 
+		g.fillOval(x1, y1, x2 - x1 + 1, y2 - y1 + 1); 
+	}
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	} 
+}
