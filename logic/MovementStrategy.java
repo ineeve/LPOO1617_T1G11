382cce4712ccf65b04f71969bc8775986717d886
styti0n <joaocarvalho9@gmail.com>
2017-03-24 23:31:50 +0000

    change package of MovementStrategy

--- logic/MovementStrategy.java
+++ logic/MovementStrategy.java
@@ -1,8 +1,6 @@
-package dkeep.cli;
+package dkeep.logic;
 
 import java.util.Random;
-
-import static dkeep.cli.UserInput.*;
 
 /**
  * Created by João on 23/02/2017.
@@ -11,10 +9,6 @@ public class MovementStrategy {
 
     private final char[] path = new char[]{'a', 's', 's', 's', 's', 'a', 'a', 'a', 'a', 'a', 'a', 's', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'w', 'w', 'w', 'w', 'w'};
     private int pathIterator = 0;
-
-    public char userMovement() {
-        return getChar();
-    }
 
     public char randomMovement() {
         Random r = new Random();
