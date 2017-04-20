a21659dc6b5d8f07ace5e68c7144ce5a43bffbd3
styti0n <joaocarvalho9@gmail.com>
2017-04-20 01:35:39 +0100

    add firebase

--- android/src/com/raiden/game/AndroidLauncher.java
+++ android/src/com/raiden/game/AndroidLauncher.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.raiden.game.RaidenMultiplayer;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -11,6 +13,12 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("IP");
+        myRef.setValue("Ola do aplicativo");
+
 		initialize(new RaidenMultiplayer(), config);
 	}
 }
