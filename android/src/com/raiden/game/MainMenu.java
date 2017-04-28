7c6ecffed140a59df862acabc71813623092f922
styti0n <joaocarvalho9@gmail.com>
2017-04-28 14:39:28 +0100

    change main menu, hide title bar

--- android/src/com/raiden/game/MainMenu.java
+++ android/src/com/raiden/game/MainMenu.java
@@ -1,22 +1,18 @@
 package com.raiden.game;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.Window;
+
+import com.badlogic.gdx.Gdx;
 
 /**
  * Created by João on 28/04/2017.
  */
 
-public class MainMenu extends AppCompatActivity implements AdapterView.OnItemClickListener{
+public class MainMenu extends AppCompatActivity implements View.OnClickListener{
 
     private static final Class[] CLASSES = new Class[]{
             LoginMenu.class,
@@ -33,52 +29,26 @@ public class MainMenu extends AppCompatActivity implements AdapterView.OnItemCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
 
-        // Set up ListView and Adapter
-        ListView listView = (ListView) findViewById(R.id.list_view);
-
-        MyArrayAdapter adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_2, CLASSES);
-        adapter.setDescriptionIds(DESCRIPTION_IDS);
-
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
+        // Button listeners
+        findViewById(R.id.login_button).setOnClickListener(this);
+        findViewById(R.id.play_button).setOnClickListener(this);
+        findViewById(R.id.exit_button).setOnClickListener(this);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Class clicked = CLASSES[position];
-        startActivity(new Intent(this, clicked));
-    }
-
-    public static class MyArrayAdapter extends ArrayAdapter<Class> {
-
-        private Context mContext;
-        private Class[] mClasses;
-        private int[] mDescriptionIds;
-
-        public MyArrayAdapter(Context context, int resource, Class[] objects) {
-            super(context, resource, objects);
-
-            mContext = context;
-            mClasses = objects;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = convertView;
-
-            if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
-                view = inflater.inflate(android.R.layout.simple_list_item_2, null);
-            }
-
-            ((TextView) view.findViewById(android.R.id.text1)).setText(mClasses[position].getSimpleName());
-            ((TextView) view.findViewById(android.R.id.text2)).setText(mDescriptionIds[position]);
-
-            return view;
-        }
-
-        public void setDescriptionIds(int[] descriptionIds) {
-            mDescriptionIds = descriptionIds;
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_button:
+                startActivity(new Intent(this, CLASSES[0]));
+                break;
+            case R.id.play_button:
+                startActivity(new Intent(this, CLASSES[1]));
+                break;
+            case R.id.exit_button:
+                Gdx.app.exit();
+                System.exit(0);
+                break;
         }
     }
+
 }
