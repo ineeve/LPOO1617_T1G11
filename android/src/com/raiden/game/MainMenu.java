4c77fe8c659a2741a1d9e5656a7f6d9cd7b3102a
styti0n <joaocarvalho9@gmail.com>
2017-04-30 01:32:25 +0100

    change UI and add multidex suport

--- android/src/com/raiden/game/MainMenu.java
+++ android/src/com/raiden/game/MainMenu.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.badlogic.gdx.Gdx;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
@@ -18,7 +17,9 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener{
 
     private static final Class[] CLASSES = new Class[]{
             LoginMenu.class,
-            PlayLauncher.class
+            PlayLauncher.class,
+            //Settings.class,
+            //HangarMenu.class
     };
 
     private static final int[] DESCRIPTION_IDS = new int[] {
@@ -33,8 +34,17 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener{
 
         // Button listeners
         findViewById(R.id.login_button).setOnClickListener(this);
+        findViewById(R.id.settings_button).setOnClickListener(this);
         findViewById(R.id.play_button).setOnClickListener(this);
+        findViewById(R.id.hangar_button).setOnClickListener(this);
         findViewById(R.id.exit_button).setOnClickListener(this);
+        findViewById(R.id.pve_singleplayer_button).setOnClickListener(this);
+        findViewById(R.id.pve_multiplayer_button).setOnClickListener(this);
+        findViewById(R.id.pvp_multiplayer_button).setOnClickListener(this);
+        findViewById(R.id.quickstart_button).setOnClickListener(this);
+        findViewById(R.id.invite_to_play_button).setOnClickListener(this);
+        findViewById(R.id.view_invitation_button).setOnClickListener(this);
+        findViewById(R.id.back_button).setOnClickListener(this);
     }
 
     @Override
@@ -43,17 +53,46 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener{
             case R.id.login_button:
                 startActivity(new Intent(this, CLASSES[0]));
                 break;
+            case R.id.settings_button:
+                break;
             case R.id.play_button:
+                findViewById(R.id.main_menu_buttons).setVisibility(view.GONE);
+                findViewById(R.id.play_menu_buttons).setVisibility(view.VISIBLE);
+                findViewById(R.id.back_button).setVisibility(view.VISIBLE);
+                break;
+            case R.id.hangar_button:
+                break;
+            case R.id.exit_button:
+                this.finish();
+                System.exit(0);
+                break;
+            case R.id.pve_singleplayer_button:
                 if (!isConnected()) {
                     GameUtils.makeSimpleDialog(this, getString(R.string.please_sign_in)).show();
                     return;
                 }
                 startActivity(new Intent(this, CLASSES[1]));
                 break;
-            case R.id.exit_button:
-                Gdx.app.exit();
-                System.exit(0);
+            case R.id.pve_multiplayer_button:
+                findViewById(R.id.play_menu_buttons).setVisibility(view.GONE);
+                findViewById(R.id.multiplayer_menu_buttons).setVisibility(view.VISIBLE);
+                //TODO: define variabel to know is a multiplayer game
                 break;
+            case R.id.pvp_multiplayer_button:
+                findViewById(R.id.play_menu_buttons).setVisibility(view.GONE);
+                findViewById(R.id.multiplayer_menu_buttons).setVisibility(view.VISIBLE);
+                //TODO: define variabel to know is a multiplayer game
+                break;
+            case R.id.back_button:
+                if(findViewById(R.id.play_menu_buttons).getVisibility() == view.VISIBLE) {
+                    findViewById(R.id.play_menu_buttons).setVisibility(view.GONE);
+                    findViewById(R.id.back_button).setVisibility(view.GONE);
+                    findViewById(R.id.main_menu_buttons).setVisibility(view.VISIBLE);
+                }
+                else if(findViewById(R.id.multiplayer_menu_buttons).getVisibility() == view.VISIBLE) {
+                    findViewById(R.id.multiplayer_menu_buttons).setVisibility(view.GONE);
+                    findViewById(R.id.play_menu_buttons).setVisibility(view.VISIBLE);
+                }
         }
     }
 
