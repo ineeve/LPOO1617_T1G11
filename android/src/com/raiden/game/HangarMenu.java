6b6356646bbe6a4abc0f9e23f3fed692c84cebf1
styti0n <joaocarvalho9@gmail.com>
2017-05-19 02:02:24 +0100

    add focus on select card & know how to add new ones

--- android/src/com/raiden/game/HangarMenu.java
+++ android/src/com/raiden/game/HangarMenu.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+
 
 /**
  * Created by João on 03/05/2017.
@@ -14,6 +16,8 @@ import android.widget.LinearLayout;
 public class HangarMenu extends AppCompatActivity implements View.OnClickListener{
     private HorizontalScrollView mScrollView;
     private LinearLayout mLinearLayout;
+
+    private ArrayList<mHangarCard> cards = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +28,55 @@ public class HangarMenu extends AppCompatActivity implements View.OnClickListene
         mLinearLayout = (LinearLayout) findViewById(R.id.mScrollLayout);
         mHangarCard image = new mHangarCard(this);
         image.setImageResource(R.drawable.settings_button);
+        image.setId(R.id.mCardHangar);
         mLinearLayout.addView(image, 0);
+        mHangarCard image2 = new mHangarCard(this);
+        image2.setImageResource(R.drawable.settings_button);
+        image2.setId(R.id.mCardHangar2);
+        mLinearLayout.addView(image2, 0);
+        cards.add(image);
+        cards.add(image2);
+        image.setOnClickListener(this);
+        image2.setOnClickListener(this);
 
     }
 
+    void setOtherCardsUnFocused(){
+        for (mHangarCard card : cards) {
+            if(card.isFocusable()) {
+                card.setFocusable(false);
+                int width = (int) (card.getWidth() / 1.2);
+                int height = (int) (card.getHeight() / 1.2);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
+                card.setLayoutParams(layoutParams);
+            }
+        }
+    }
+
+    void setOneCardFocused(View v){
+        v.setFocusable(true);
+        int width = (int) (v.getWidth() * 1.2);
+        int height = (int) (v.getHeight() * 1.2);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
+        v.setLayoutParams(layoutParams);
+    }
+
+    void setOneCardUnFocused(View v){
+        v.setFocusable(false);
+        int width = (int) (v.getWidth() / 1.2);
+        int height = (int) (v.getHeight() / 1.2);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
+        v.setLayoutParams(layoutParams);
+    }
 
     @Override
     public void onClick(View v) {
-
+        if(!v.isFocusable()){
+            setOtherCardsUnFocused();
+            setOneCardFocused(v);
+        }
+        else{
+            setOneCardUnFocused(v);
+        }
     }
 }
--- android/src/com/raiden/game/mHangarCard.java
+++ android/src/com/raiden/game/mHangarCard.java
@@ -1,13 +1,16 @@
 package com.raiden.game;
 
 import android.content.Context;
+import android.widget.LinearLayout;
 
-/**
- * Created by João on 19/05/2017.
- */
 
 public class mHangarCard extends android.support.v7.widget.AppCompatImageView {
     public mHangarCard(Context context) {
         super(context);
+        this.setBackgroundResource(R.drawable.menu_buttons);
+        int width = (int) getResources().getDimension(R.dimen.cardhanger_width);
+        int height = (int) getResources().getDimension(R.dimen.cardhanger_height);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
+        this.setLayoutParams(layoutParams);
     }
 }
