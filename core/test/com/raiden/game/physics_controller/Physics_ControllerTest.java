b7edffe5b3a281d0457203c71335b8e294e232cf
ineeve <renatoineeve@gmail.com>
2017-06-05 17:29:04 +0100

    Fixed junit dependecies

--- core/test/com/raiden/game/physics_controller/Physics_ControllerTest.java
+++ core/test/com/raiden/game/physics_controller/Physics_ControllerTest.java
@@ -1,11 +1,5 @@
 package com.raiden.game.physics_controller;
 
-import com.raiden.game.model.entities.Airplane_1_Model;
-import com.raiden.game.model.entities.EntityModel;
-import com.raiden.game.model.entities.ShipModel;
-import com.raiden.game.physics_controller.entities.AirPlane_1;
-import com.raiden.game.physics_controller.entities.ShipBody;
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -18,8 +12,6 @@ public class Physics_ControllerTest extends GameTest {
     public void testBodyPosition(){
         Physics_Controller controller = Physics_Controller.getInstance();
         assertNotNull(controller);
-        ShipModel shipModel = new Airplane_1_Model(10,10);
-        ShipBody shipBody = new AirPlane_1(controller.getWorld(),shipModel);
 
     }
 
